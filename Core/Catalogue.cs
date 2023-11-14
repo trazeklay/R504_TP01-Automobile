09930792d0529c218ab1a7b4294e4fe94da1485a
clement trompier <clem.trompier@gmail.com>
2023-11-14 08:19:57 +0100

    Filled Catalogue

--- Core/Catalogue.cs
+++ Core/Catalogue.cs
@@ -1,4 +1,6 @@
 ﻿using R504_TP01_Automobile.Factories;
+using R504_TP01_Automobile.Core.Scooter;
+using R504_TP01_Automobile.Core.Car;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +11,21 @@ namespace R504_TP01_Automobile.Core
 {
     public class Catalogue
     {
-        public IVehicleFactory vehicleFactory;
+        public ElectricVehicleFactory electricVehicleFactory;
+        public PetrolVehicleFactory petrolVehicleFactory;
 
+        public List<PetrolCar> petrolCars { get; set; }
+        public List<ElectricCar> electricCars { get; set; }
 
+        public List<PetrolScooter> petrolScooters { get; set; }
+        public List<ElectricScooter> electricScooters { get; set; }
+
+        public Catalogue()
+        {
+            this.petrolCars = new List<PetrolCar>();
+            this.electricCars = new List<ElectricCar>();
+            this.petrolScooters = new List<PetrolScooter>();
+            this.electricScooters = new List<ElectricScooter>();
+        }
     }
 }
